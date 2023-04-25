0522186342b2d044fc5f3d39495efca96b02da10
Mara Daniela López Pérez <m.lopezp171003@gmail.com>
2023-04-24 22:48:39 -0600

    Clase castillo

---
+++ src/towerdefense/Castillo.java
@@ -0,0 +1,22 @@
+package towerdefense;
+
+/**
+ *
+ * @author Marita
+ */
+public class Castillo {
+
+        private int vida;
+
+        public Castillo() {
+            vida = 10;
+        }
+
+        public int getVida() {
+            return vida;
+        }
+
+        public void recibirAtaque(int ataque) {
+            vida -= ataque;
+        }
+    }
