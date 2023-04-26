2593e07f615be2a8f49ee1e18c73946f6c78d03b
racatal <catvalra@gmail.com>
2023-04-26 16:51:08 -0600

    Intento de resolver el error String cannot be converted to Tropa

--- src/towerdefense/Camino.java
+++ src/towerdefense/Camino.java
@@ -9,10 +9,10 @@ public class Camino {
     private Tropa[] tropas;
     
     public Camino() {
-        tropas = new Tropa[10];
+        tropas = new Tropa[10]; //ongitud de 10
     }
     
-    public void colocarTropa(Tropa tropa, int posicion) {
+    public void colocarTropa(Tropa tropa, int posicion) { //colocar tropa en una posicion en especifico
         tropas[posicion] = tropa;
     }
     
